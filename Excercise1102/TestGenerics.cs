3c0a18b476b6e7fbd339a61a386955fbf5d12dea
Voxuel <Leo.fridh@hotmail.com>
2022-11-02 13:25:21 +0100

    added xml

--- Excercise1102/TestGenerics.cs
+++ Excercise1102/TestGenerics.cs
@@ -6,6 +6,13 @@ namespace Excercise1102
 {
     internal class TestGenerics
     {
+        /// <summary>
+        /// Compares 2 objects and returns true or false depending on what Equals() returns
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>true if both objeccts are equals</returns>
         internal bool Compare<T>(T x, T y)
         {
             return x.Equals(y);
