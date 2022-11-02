960827317054e4bd7f685cff414b29fe19c873df
Voxuel <Leo.fridh@hotmail.com>
2022-11-02 13:15:32 +0100

    Add project files.

---
+++ Excercise1102/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Excercise1102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = new TestGenerics();
+
+            bool result = test1.Compare(1, 4);
+
+            Console.WriteLine(result ? "They are equals" : "They are not equals");
+
+            var test2 = new TestGenerics();
+
+            bool result2 = test2.Compare("Hej", "Hej");
+
+            Console.WriteLine(result2 ? "They are equals" : "They are not equals");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Excercise1102/TestGenerics.cs
@@ -0,0 +1,14 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excercise1102
+{
+    internal class TestGenerics
+    {
+        internal bool Compare<T>(T x, T y)
+        {
+            return x.Equals(y);
+        }
+    }
+}
