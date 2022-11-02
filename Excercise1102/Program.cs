b78ccbb60d0270099e527012aeff5b60bfdf074f
Voxuel <Leo.fridh@hotmail.com>
2022-11-02 13:19:15 +0100

    update

--- Excercise1102/Program.cs
+++ Excercise1102/Program.cs
@@ -8,15 +8,11 @@ namespace Excercise1102
         {
             var test1 = new TestGenerics();
 
-            bool result = test1.Compare(1, 4);
-
-            Console.WriteLine(result ? "They are equals" : "They are not equals");
+            Console.WriteLine(test1.Compare(1,4) ? "They are equals" : "They are not equals");
 
             var test2 = new TestGenerics();
 
-            bool result2 = test2.Compare("Hej", "Hej");
-
-            Console.WriteLine(result2 ? "They are equals" : "They are not equals");
+            Console.WriteLine(test2.Compare("Hej","Hej") ? "They are equals" : "They are not equals");
 
             Console.ReadLine();
         }
